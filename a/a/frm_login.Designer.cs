3e3490fbec180bd6945e219b4250f0f21d75b049
Luka Gajdek <lgajdek20@foi.hr>
2023-04-25 17:48:17 +0200

    Završena login forma

---
+++ a/a/frm_login.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace a
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_kor_ime = new System.Windows.Forms.TextBox();
+            this.tb_lozinka = new System.Windows.Forms.TextBox();
+            this.btn_prijava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Korisničko ime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lozinka";
+            // 
+            // tb_kor_ime
+            // 
+            this.tb_kor_ime.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_kor_ime.Location = new System.Drawing.Point(311, 30);
+            this.tb_kor_ime.Name = "tb_kor_ime";
+            this.tb_kor_ime.Size = new System.Drawing.Size(371, 45);
+            this.tb_kor_ime.TabIndex = 2;
+            // 
+            // tb_lozinka
+            // 
+            this.tb_lozinka.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_lozinka.Location = new System.Drawing.Point(311, 102);
+            this.tb_lozinka.Name = "tb_lozinka";
+            this.tb_lozinka.Size = new System.Drawing.Size(371, 45);
+            this.tb_lozinka.TabIndex = 3;
+            this.tb_lozinka.UseSystemPasswordChar = true;
+            // 
+            // btn_prijava
+            // 
+            this.btn_prijava.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_prijava.Location = new System.Drawing.Point(495, 188);
+            this.btn_prijava.Name = "btn_prijava";
+            this.btn_prijava.Size = new System.Drawing.Size(187, 67);
+            this.btn_prijava.TabIndex = 4;
+            this.btn_prijava.Text = "Prijava";
+            this.btn_prijava.UseVisualStyleBackColor = true;
+            this.btn_prijava.Click += new System.EventHandler(this.btn_prijava_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 291);
+            this.Controls.Add(this.btn_prijava);
+            this.Controls.Add(this.tb_lozinka);
+            this.Controls.Add(this.tb_kor_ime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prijava u sustav";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_kor_ime;
+        private System.Windows.Forms.TextBox tb_lozinka;
+        private System.Windows.Forms.Button btn_prijava;
+    }
+}
+
---
+++ a/a/frm_login.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace a
+{
+    public partial class frm_login : Form
+    {
+        public frm_login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_prijava_Click(object sender, EventArgs e)
+        {
+            string kor_ime = tb_kor_ime.Text;
+            string lozinka = tb_lozinka.Text;
+
+            if(kor_ime == "lgajdek20" && lozinka == "sifra")
+            {
+                MessageBox.Show("Uspješna prijava!", "Login");
+            }
+
+            else
+            {
+                MessageBox.Show("Neuspješna prijava!\nPogrešna lozinka ili username", "Login");
+            }
+        }
+    }
+}
